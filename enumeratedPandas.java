c981cb4d02b7a54d771c204cf86343df020619aa
Iggysaur <eswaind@gmail.com>
2020-04-07 09:10:11 -0400

    Add files via upload

---
+++ enumeratedPandas.java
@@ -0,0 +1,21 @@
+package BoredPanda;
+
+
+public class enumeratedPandas {
+
+    public enum Activity{
+
+    }
+
+    public enum Names{
+
+    }
+
+    public enum Quality{
+
+    }
+
+    public enum Luck {
+
+    }
+}
